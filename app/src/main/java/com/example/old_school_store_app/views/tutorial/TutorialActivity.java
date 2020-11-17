c77805adcec8be2b9d3eaa4c7adc75a1dd91cb4f
Alexei Smyslov <a.m.smyslov@yandex.ru>
2020-11-17 18:46:38 +0300

    view flipper done

--- app/src/main/java/com/example/old_school_store_app/views/tutorial/TutorialActivity.java
+++ app/src/main/java/com/example/old_school_store_app/views/tutorial/TutorialActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.AnimationUtils;
 import android.widget.ViewFlipper;
 
@@ -19,13 +20,22 @@ public class TutorialActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();//отключил верхнюю плашку
         setContentView(R.layout.activity_tutorial);
 
         viewFlipperPages = findViewById(R.id.viewFlipperPages);
 
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        int[] layoutIds = new int[]{R.layout.test, R.layout.first_page, R.layout.first_page};
+        int[] layoutIds = new int[]{
+                R.layout.first_page,
+                R.layout.second_page,
+                R.layout.third_page,
+                R.layout.fourth_page,
+                R.layout.fifth_page,
+                R.layout.sixth_page,
+                R.layout.seventh_page,
+        };
 
         for (int i = 0; i < layoutIds.length; i++)
         {
