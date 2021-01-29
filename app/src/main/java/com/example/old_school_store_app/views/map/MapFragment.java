86583ebae8cbf03ee370da42cc1520c71628fd79
Alexei Smyslov <a.m.smyslov@yandex.ru>
2021-01-29 19:00:47 +0300

    done map with alert dialogs

--- app/src/main/java/com/example/old_school_store_app/views/map/MapFragment.java
+++ app/src/main/java/com/example/old_school_store_app/views/map/MapFragment.java
@@ -1,18 +1,26 @@
 package com.example.old_school_store_app.views.map;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.old_school_store_app.R;
 import com.example.old_school_store_app.models.DataStorage;
+import com.example.old_school_store_app.views.main.MainActivity;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.MapObject;
 import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.MapObjectTapListener;
 import com.yandex.mapkit.map.PlacemarkMapObject;
 import com.yandex.mapkit.mapview.MapView;
 import com.yandex.runtime.image.ImageProvider;
@@ -46,14 +54,21 @@ public class MapFragment extends Fragment
         PlacemarkMapObject point1 = mapObjects.addPlacemark(new Point(53.243880, 34.362213));
         point1.setOpacity(0.5f);
         point1.setIcon(ImageProvider.fromResource(context, R.drawable.mark2));
+        point1.setUserData("Точка 1. Любят девочек.\nЧасы работы: пн-вск: 10:00-19:00\nТелефон:+7(999)299-88-99");
+        point1.addTapListener(pointMapObjectTapListener);
+
 
         PlacemarkMapObject point2 = mapObjects.addPlacemark(new Point(53.275516, 34.316101));
         point2.setOpacity(0.5f);
         point2.setIcon(ImageProvider.fromResource(context, R.drawable.mark2));
+        point2.setUserData("Точка 2. Любят девочек.\nЧасы работы: пн-вск: 10:00-19:00\nТелефон:+7(999)299-88-99");
+        point2.addTapListener(pointMapObjectTapListener);
 
         PlacemarkMapObject point3 = mapObjects.addPlacemark(new Point(53.308033, 34.301776));
         point3.setOpacity(0.5f);
         point3.setIcon(ImageProvider.fromResource(context, R.drawable.mark2));
+        point3.setUserData("Точка 3. Любят девочек.\nЧасы работы: пн-вск: 10:00-19:00\nТелефон:+7(999)299-88-99");
+        point3.addTapListener(pointMapObjectTapListener);
     }
 
     @Override
@@ -69,4 +84,27 @@ public class MapFragment extends Fragment
         MapKitFactory.getInstance().onStart();
         mapView.onStart();
     }
+
+    private MapObjectTapListener pointMapObjectTapListener = new MapObjectTapListener() {
+        @Override
+        public boolean onMapObjectTap(@NonNull MapObject mapObject, @NonNull Point point)
+        {
+            String message = (String) mapObject.getUserData();
+
+            MainActivity mainActivity = (MainActivity) DataStorage.Get("mainActivity");
+
+            AlertDialog.Builder dialogWindow  = new AlertDialog.Builder(mainActivity);
+            dialogWindow.setMessage(message);
+            dialogWindow.setTitle("Информация о магазине");
+            dialogWindow.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            dialogWindow.setCancelable(true);
+            dialogWindow.create().show();
+
+            return true;
+        }
+    };
+
 }
